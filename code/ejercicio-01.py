a463750dd08a90d37b6ceadc10d2e04c25dd2946
MiguelCarvajal <macg4202@gmail.com>
2026-09-17 20:51:10 -0500

    se crea primer ejercicio de hilos

---
+++ code/ejercicio-01.py
@@ -0,0 +1,17 @@
+import threading
+import time
+
+def imprimir_mensaje():
+    for i in range(5):
+        print(f"{i} Hello")
+        time.sleep(1)
+
+def main():
+    thread = threading.Thread(target=imprimir_mensaje)
+    thread.start()
+    thread.join()
+    print("Finalizo")
+
+if __name__ == "__main__":
+    main()
+
